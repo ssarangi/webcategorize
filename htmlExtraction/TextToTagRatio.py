ba2959475701db57ba835b44078ebf0b73bffc69
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2011-08-24 00:22:24 -0400

    Initial Tag ratio code

--- htmlExtraction/TextToTagRatio.py
+++ htmlExtraction/TextToTagRatio.py
@@ -1,84 +1,82 @@
-from lxml import etree
 import string
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
 from lxml.html import clean
 from HTMLParser import HTMLParser
-from formatter import *
+import numpy as np
 
-class TrackingParser(HTMLParser):
+class TagParser(HTMLParser):
     """Try to keep accurate pointer of parsing location."""
     def __init__(self):
         HTMLParser.__init__(self)
+        self.tag_chars = 0
+        self.nontag_chars = 0
+
+    def handle_starttag(self, tag, attrs):
+        print "Found start tag: %s" % tag
+        # accumulate the length of the attrs
+        attr_len = 0
+        for attr in attrs:
+            attr_len += len(attr)
         
-    def parse_starttag(self, i):
-        print "Parsing Start Tag"
-        index = HTMLParser.parse_starttag(self, i)
-        print "Index found: %s" % index
-        return index
-
-    def parse_endtag(self, i):
-        print "Parsing End Tag"
-        index = HTMLParser.parse_endtag(self, i)
-        print "Index Found: %s" % index
-
-class HtmlTokenParser:
-    def __init__(self, html, removal_tag_list = []):
-        # Clean the HTML
+        # Now count the number of attribute values
+        total_attrs = len(attrs) / 2
+        attr_len += total_attrs  # Add the length of '=' removed by the parser
+        
+        tag_len = len(tag) + attr_len
+        self.tag_chars += tag_len
+    
+    def handle_endtag(self, tag):
+        print "Found end tag: %s" % tag
+        self.tag_chars += len(tag)
+        
+    def handle_data(self, data):
+        if (data.strip() != ""):
+            print "Found data: %s" % data
+            self.nontag_chars += len(data)
+        
+class TextToTagRatio:
+    def __init__(self, html_file, removal_tag_list = []):
+        ''' Initialize the Tag statistics '''
+        # clean the html
+        self.fptr = open(html_file, 'r')
+        self.html = html
         cleaner = clean.Cleaner()
         cleaner.remove_tags = removal_tag_list
-        # cleaned_html = cleaner.clean_html(html)
-        cleaned_html = html
+        cleaned_html = cleaner.clean_html(self.html)
+        self.html = cleaned_html
+        self.html_list = string.split(self.html, '\n')    
+        self.parser = TagParser()
+        self.line_stats = []
         
-        # Dictionary of line numbers & tag chars
-        self.text_to_tag_ratio = {}
-        self.html_tree = etree.fromstring(cleaned_html)        
-        
-    def remove_tag(self, tag):                        
-        for itag in self.html_tree.getiterator():
-            if (itag.tag == tag):
-                print "hello"
-                # itag.drop_tree()
-
-    def remove_empty_lines(self):
-        for itag in self.html_tree.getiterator():
-            if (itag.text != None and string.strip(itag.text) == ""):
-                print "Hello"
-                # itag.drop_tag()
+    def analyze_html(self):
+        line_no = 0
+        for line in self.html_list:
+            self.parser.feed(line)
+            self.parser.reset()
+            self.line_stats.append(0)
+            self.line_stats[line_no] = float(self.parser.tag_chars) / float(self.parser.nontag_chars)
+            line_no += 1
             
-    def display_tree(self):
-        for element in self.html_tree.getiterator():
-            print element.tag
+        self.parser.close()
+        
+    def display_results(self):
+        for e in self.line_stats:
+            print e
     
-    def display_html_content(self):
-        print etree.tostring(self.html_tree, pretty_print = True)
-        
-    def generate_statistics(self):
-        for node in self.html_tree.iter():
-            if (node.text != None):
-                text = node.text
-                
-            if (node.tail != None):
-                text += node.tail
-                
-            text_length = len(text)
-            self.text_to_tag_ratio[node.tag] = text_length
-        
-    def display_statistics(self):
-        for key,val in self.text_to_tag_ratio.items():
-            print "Key: %s Value: %s" % (key, val)
-            
+    def plot_graph(self):
+        r = range(0, len(self.line_stats) - 1)
+        twoD = np.array([r, self.line_stats])
+        n, bins, patches = plt.hist(twoD, len(self.line_stats), normed=1, facecolor='g', alpha=0.75)
+        plt.grid(True)
+        plt.show()
         
 if __name__ == "__main__":
     fptr = open('test.html', 'r')
     html = fptr.read()
     
-    # remove_tags = ["script", "remark"]
-    # parser = HtmlTokenParser(html, remove_tags)
-    # parser.remove_empty_lines()
-    # parser.generate_statistics()
-    # parser.display_statistics()  
-    parser = TrackingParser()
-    parser.feed(html)
-    parser.close()
-    
-    # parser.display_tree()
-    # parser.display_html_content()
+    remove_tags = ["script", "remark"]
+
+    TTR = TextToTagRatio('test.html', remove_tags)
+    TTR.analyze_html()
+    TTR.plot_graph()
