5e8f4c8af9d03dee17345c8aa8e355e99cc7e4e2
Satyajit Sarangi <ssarangi123@gmail.com>
2011-09-11 18:41:26 -0400

    Added DB support

--- htmlExtraction/keyword_parse.py
+++ htmlExtraction/keyword_parse.py
@@ -5,9 +5,20 @@ import os
 db_filename = 'keywords.db'
 schema_filename = 'keywords_schema.sql'
 db_is_new = not os.path.exists(db_filename)
+conn = sqlite3.connect(db_filename)
+
+def init_db():
+    global conn, schema_filename, db_is_new
+    if db_is_new:
+        print 'Creating schema'
+        with open(schema_filename, 'rt') as f:
+            schema = f.read()
+        conn.executescript(schema)    
 
 def remove_text_from_string(orig_str, to_be_removed_str):
-    return orig_str[len(to_be_removed_str):]
+    new_str = orig_str[len(to_be_removed_str):]
+    new_str = new_str.replace("'", " ").strip()
+    return new_str
 
 def remove_empty_rows(column):
     final_ls = []
@@ -29,24 +40,15 @@ def reparse_keywords(keywords_column):
     
     return new_column
 
-def init_connection():
-    global db_filename
-    with sqlite3.connect(db_filename) as conn:
-        if db_is_new:
-            print 'Creating schema'
-            with open(schema_filename, 'rt') as f:
-                schema = f.read()
-                conn.executescript(schema)
-        else:
-            print 'Database exists, assume schema does, too.'
-
-
-def write_table(table_name, column_name, data_list):
-    global db_filename
-    conn = sqlite3.connect(db_filename)
-    
-    sql_query = """insert into %s (%s) values('%s')""" % (table_name, column_name, data)
-
+def write_table(table_name, column_name, data_set):
+    global conn
+    id = 0
+    for d in data_set:
+        sql_query = """insert into %s (id, %s) values('%s', '%s')""" % (table_name, column_name, str(id), d)
+        id += 1
+        # print sql_query
+        conn.execute(sql_query)
+        
 def read_excel(filename):
     ''' sl are service lines '''
     wb = open_workbook(filename)
@@ -57,15 +59,37 @@ def read_excel(filename):
     sl3 = sheet0.col_slice(5,2)
     keywords = sheet0.col_slice(10, 2)
 
-    sl1 = set(remove_empty_rows(sl1))
-    sl2 = set(remove_empty_rows(sl2))
-    sl3 = set(remove_empty_rows(sl3))
+    sl1 = set([remove_text_from_string(s, "text:u'") for s in remove_empty_rows(sl1)])
+    sl2 = set([remove_text_from_string(s, "text:u'") for s in remove_empty_rows(sl2)])
+    sl3 = set([remove_text_from_string(s, "text:u'") for s in remove_empty_rows(sl3)])
     keywords_column = remove_empty_rows(keywords)
     
     keywords_column = reparse_keywords(keywords_column)
-            
-    for keyword in keywords_column:
-        print keyword
-        
+
+    write_table('service_line_1', 'sl1_keyword', sl1)
+    write_table('service_line_2', 'sl2_keyword', sl1)
+    write_table('service_line_3', 'sl3_keyword', sl1)
+    
+    # serialize the keyword column
+    serialized_col = []
+    for klist in keywords_column:
+        for k in klist:
+            serialized_col.append(k)
+    
+    serialized_col = set(serialized_col)
+    write_table('keyword_table', 'keyword', serialized_col)
+    
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    select * from keyword_table
+    """)
+
+    for row in cursor.fetchall():
+        id, keyword = row
+        print '%s: %s' % (id, keyword)    
+    
 if __name__ == "__main__":
+    init_db()
     read_excel('automapping.xls')
--- htmlExtraction/keywords_schema.sql
+++ htmlExtraction/keywords_schema.sql
@@ -1,25 +1,25 @@
 create table service_line_1 (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY autoincrement not null,
     sl1_keyword TEXT NOT NULL UNIQUE
 );
 
 create table service_line_2 (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY autoincrement not null,
     sl2_keyword TEXT NOT NULL UNIQUE
 );
 
 create table service_line_3 (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY autoincrement not null,
     sl3_keyword TEXT NOT NULL UNIQUE
 );
 
-create table keyword (
-    id INTEGER PRIMARY KEY,
+create table keyword_table (
+    id INTEGER PRIMARY KEY autoincrement not null,
     keyword TEXT NOT NULL UNIQUE
 );
 
 create table relationship (
-  id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY autoincrement not null,
   sl3_index INTEGER,
   sl2_index INTEGER,
   sl1_index INTEGER,
@@ -27,6 +27,6 @@ create table relationship (
   FOREIGN KEY(sl3_index) REFERENCES service_line_3(id),
   FOREIGN KEY(sl2_index) REFERENCES service_line_2(id),
   FOREIGN KEY(sl1_index) REFERENCES service_line_1(id),
-  FOREIGN KEY(keyword_index) REFERENCES keywords(id));
+  FOREIGN KEY(keyword_index) REFERENCES keywords(id)
 );
     
