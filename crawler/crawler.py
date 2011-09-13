2a0d681a2c53c904bdd6b25f0b3b27dcf2a8cb73
Satyajit Sarangi <ssarangi123@gmail.com>
2011-09-12 20:49:13 -0400

    Added main.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,7 +3,7 @@
 """Web Crawler/Spider
 
 This module implements a web crawler. This is very _basic_ only
-and needs to be extended to do anything usefull with the
+and needs to be extended to do anything useful with the
 traversed pages.
 
 From: http://code.activestate.com/recipes/576551-simple-web-crawler/
@@ -30,9 +30,6 @@ __copyright__ = "CopyRight (C) 2008-2011 by Satyajit Sarangi. Originally by Jame
 __license__ = "MIT"
 __author__ = "Satyajit Sarangi"
 __author_email__ = "Satyajit Sarangi, Satyajit dot Sarangi at gmail dot com"
-
-USAGE = "%prog [options] <url>"
-VERSION = "%prog v" + __version__
 
 AGENT = "%s/%s" % (__name__, __version__)
 
@@ -255,72 +252,6 @@ class Fetcher(object):
                     if url not in self:
                         self.out_urls.append(url)
 
-def getLinks(url):
-    page = Fetcher(url)
-    page.fetch()
-    for i, url in enumerate(page):
-        print "%d. %s" % (i, url)
-
-def parse_options():
-    """parse_options() -> opts, args
-
-    Parse any command-line options given returning both
-    the parsed options and arguments.
-    """
-
-    parser = optparse.OptionParser(usage=USAGE, version=VERSION)
-
-    parser.add_option("-q", "--quiet",
-            action="store_true", default=False, dest="quiet",
-            help="Enable quiet mode")
-
-    parser.add_option("-l", "--links",
-            action="store_true", default=False, dest="links",
-            help="Get links for specified url only")    
-
-    parser.add_option("-d", "--depth",
-            action="store", type="int", default=30, dest="depth_limit",
-            help="Maximum depth to traverse")
-
-    parser.add_option("-c", "--confine",
-            action="store", type="string", dest="confine",
-            help="Confine crawl to specified prefix")
-
-    parser.add_option("-x", "--exclude", action="append", type="string",
-                      dest="exclude", default=[], help="Exclude URLs by prefix")
-    
-    parser.add_option("-L", "--show-links", action="store_true", default=False,
-                      dest="out_links", help="Output links found")
-
-    parser.add_option("-u", "--show-urls", action="store_true", default=False,
-                      dest="out_urls", help="Output URLs found")
-
-    parser.add_option("-D", "--dot", action="store_true", default=False,
-                      dest="out_dot", help="Output Graphviz dot file")
-    
-
-
-    opts, args = parser.parse_args()
-
-    if len(args) < 1:
-        parser.print_help(sys.stderr)
-        raise SystemExit, 1
-
-    if opts.out_links and opts.out_urls:
-        parser.print_help(sys.stderr)
-        parser.error("options -L and -u are mutually exclusive")
-
-    # Check the URL first
-    url = args[0]
-
-    if (len(url) > 8):
-        index_http = find(url, "http://", 0, 7)
-        index_https = find(url, "http://", 0, 8)
-
-        if (index_http == -1 and index_https == -1):
-            url = "http://" + url
-
-    return opts, url, args
 
 class DotWriter:
 
@@ -359,44 +290,3 @@ class DotWriter:
         for l in links:            
             print "\t" + self._safe_alias(l.src) + " -> " + self._safe_alias(l.dst) + ";"
         print  "}"
-
-        
-    
-
-def main():    
-    opts, url, args = parse_options()
-
-    if opts.links:
-        getLinks(url)
-        raise SystemExit, 0
-
-    depth_limit = opts.depth_limit
-    confine_prefix=opts.confine
-    exclude=opts.exclude
-
-    sTime = time.time()
-
-    print >> sys.stderr,  "Crawling %s (Max Depth: %d)" % (url, depth_limit)
-    crawler = Crawler(url, depth_limit, confine_prefix, exclude)
-    crawler.crawl()
-
-    if opts.out_urls:
-        print "\n".join(crawler.urls_seen)
-
-    if opts.out_links:
-        print "\n".join([str(l) for l in crawler.links_remembered])
-        
-    if opts.out_dot:
-        d = DotWriter()
-        d.asDot(crawler.links_remembered)
-
-    eTime = time.time()
-    tTime = eTime - sTime
-
-    print >> sys.stderr, "Found:    %d" % crawler.num_links
-    print >> sys.stderr, "Followed: %d" % crawler.num_followed
-    print >> sys.stderr, "Stats:    (%d/s after %0.2fs)" % (
-            int(math.ceil(float(crawler.num_links) / tTime)), tTime)
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,110 @@
+import optparse
+import sys
+from string import *
+from crawler.crawler import *
+
+__version__ = "0.1"
+USAGE = "%prog [options] <url>"
+VERSION = "%prog v" + __version__
+
+def getLinks(url):
+    page = Fetcher(url)
+    page.fetch()
+    for i, url in enumerate(page):
+        print "%d. %s" % (i, url)
+
+def parse_options():
+    """parse_options() -> opts, args
+
+    Parse any command-line options given returning both
+    the parsed options and arguments.
+    """
+
+    parser = optparse.OptionParser(usage=USAGE, version=VERSION)
+
+    parser.add_option("-q", "--quiet",
+            action="store_true", default=False, dest="quiet",
+            help="Enable quiet mode")
+
+    parser.add_option("-l", "--links",
+            action="store_true", default=False, dest="links",
+            help="Get links for specified url only")    
+
+    parser.add_option("-d", "--depth",
+            action="store", type="int", default=30, dest="depth_limit",
+            help="Maximum depth to traverse")
+
+    parser.add_option("-c", "--confine",
+            action="store", type="string", dest="confine",
+            help="Confine crawl to specified prefix")
+
+    parser.add_option("-x", "--exclude", action="append", type="string",
+                      dest="exclude", default=[], help="Exclude URLs by prefix")
+    
+    parser.add_option("-L", "--show-links", action="store_true", default=False,
+                      dest="out_links", help="Output links found")
+
+    parser.add_option("-u", "--show-urls", action="store_true", default=False,
+                      dest="out_urls", help="Output URLs found")
+
+    parser.add_option("-D", "--dot", action="store_true", default=False,
+                      dest="out_dot", help="Output Graphviz dot file")
+    
+
+
+    opts, args = parser.parse_args()
+
+    if len(args) < 1:
+        parser.print_help(sys.stderr)
+        raise SystemExit, 1
+
+    if opts.out_links and opts.out_urls:
+        parser.print_help(sys.stderr)
+        parser.error("options -L and -u are mutually exclusive")
+
+    # Check the URL first
+    url = args[0]
+
+    if (len(url) > 8):
+        index_http = find(url, "http://", 0, 7)
+        index_https = find(url, "http://", 0, 8)
+
+        if (index_http == -1 and index_https == -1):
+            url = "http://" + url
+
+    return opts, url, args
+
+def main():    
+    opts, url, args = parse_options()
+
+    if opts.links:
+        getLinks(url)
+        raise SystemExit, 0
+
+    depth_limit = opts.depth_limit
+    confine_prefix=opts.confine
+    exclude=opts.exclude
+
+    sTime = time.time()
+
+    print >> sys.stderr,  "Crawling %s (Max Depth: %d)" % (url, depth_limit)
+    crawler = Crawler(url, depth_limit, confine_prefix, exclude)
+    crawler.crawl()
+
+    if opts.out_urls:
+        print "\n".join(crawler.urls_seen)
+
+    if opts.out_links:
+        print "\n".join([str(l) for l in crawler.links_remembered])
+        
+    if opts.out_dot:
+        d = DotWriter()
+        d.asDot(crawler.links_remembered)
+
+    eTime = time.time()
+    tTime = eTime - sTime
+
+    print >> sys.stderr, "Found:    %d" % crawler.num_links
+    print >> sys.stderr, "Followed: %d" % crawler.num_followed
+    print >> sys.stderr, "Stats:    (%d/s after %0.2fs)" % (
+            int(math.ceil(float(crawler.num_links) / tTime)), tTime)
